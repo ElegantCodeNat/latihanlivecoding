4be77f4a8c497e8a1f51ec9b993eb20095355229
ElegantCodeNat <natalia.shn94@gmail.com>
2024-09-03 19:00:05 +0700

    soal2

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ console.log(checkNumber(10));
 console.log(checkNumber(-5)); 
 console.log(checkNumber(0)); 
 
+// Latihan 2: Buatlah fungsi bernama getGrade yang menerima satu parameter score. Fungsi ini harus mengembalikan:
+
+// "A" jika score antara 90 dan 100,
+// "B" jika score antara 80 dan 89,
+// "C" jika score antara 70 dan 79,
+// "D" jika score antara 60 dan 69,
+// "F" jika score kurang dari 60.
+
+function getGrade(score){
+    if (score >= 90 && score <= 100){
+        return "A";
+    }else if (score >= 80 && score < 90){
+        return "B";
+    }else if (score >= 70 && score < 80){
+        return "C";
+    }
+    else if (score >= 60 && score < 70){
+        return "D";
+    }
+    else {
+        return "F";
+    }
+}
+
+console.log(getGrade(95)); //A
+console.log(getGrade(80)); //B
+console.log(getGrade(79)); //C
+console.log(getGrade(65)); //D
+console.log(getGrade(50)); //F
